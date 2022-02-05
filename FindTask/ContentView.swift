62faf31f6a15d9f2766f6dc242c7b351a67c719a
Dixon Chu <dixonchu.work@gmail.com>
2022-02-05 00:25:46 +0000

    Initial Commit

---
+++ FindTask/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FindTask
+//
+//  Created by Dixon Chu on 05/02/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FindTask/FindTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  FindTaskApp.swift
+//  FindTask
+//
+//  Created by Dixon Chu on 05/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct FindTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
